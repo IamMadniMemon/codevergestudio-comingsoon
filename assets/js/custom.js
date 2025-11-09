d73b6a6df7f34558d2500fd942cb83b8fb70524b
PHP/WordPress Developer <82670239+IamMadniMemon@users.noreply.github.com>
2025-11-10 03:09:31 +0500

    Update custom.js

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,4 +2,14 @@ $(document).ready(function(){
   $('#myFlipper').flipper('init');
   $('#modalFlipper').flipper('init');
 
-})
+
+  //gsap animation
+  let $tl = gsap.timeline();
+  $tl.from("h1, p.content, #myFlipper", {
+      duration: 1,     // seconds (no "s")
+      opacity: 0,
+      y: -50,
+      stagger: 0.8,
+  });
+  
+});
